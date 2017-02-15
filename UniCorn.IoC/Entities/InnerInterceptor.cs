3c7c951b243d03977d484066f3e06a9231837080
kenan hancer <kenanhancer@gmail.com>
2017-02-15 16:45:42 +0300

    UPDATED

---
+++ UniCorn.IoC/Entities/InnerInterceptor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniCorn.IoC
+{
+    public class InnerInterceptor : IInterceptor
+    {
+        Action<UniCorn.IoC.IInvocation> interceptor;
+
+        public InnerInterceptor(Action<UniCorn.IoC.IInvocation> interceptor)
+        {
+            this.interceptor = interceptor;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            if (interceptor != null)
+                interceptor(invocation);
+            else
+                invocation.Proceed();
+        }
+    }
+}
